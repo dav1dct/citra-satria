71817761d5673105e24acbb85dd3cfef966cc88e
dav1dct <lindahwatih215@gmail.com>
2025-04-24 19:05:37 +0700

    Customization and looks cleanup

--- resources/views/admin/karyawan/index.blade.php
+++ resources/views/admin/karyawan/index.blade.php
@@ -10,9 +10,10 @@
     @endif
     <a href="{{ route('karyawan.create') }}" class="btn btn-primary mb-3">Tambah Karyawan</a>
 
-    <table class="table table-bordered table-dark border-2 border-blue-500">
-        <thead class="table-dark text-center fw-bold fs-5">
+    <table class="table table-bordered table-dark border-2" style="border: 2px solid #0d6efd;">
+        <thead class="table-dark text-center fw-bold fs-5" style="border: 2px solid #0d6efd;">
             <tr>
+                <th style="width: 60px">No</th>
                 <th>Nama</th>
                 <th>Email</th>
                 <th>Posisi</th>
@@ -22,13 +23,14 @@
             </tr>
         </thead>
         <tbody>
-            @foreach($karyawans as $k)
+            @foreach($karyawans as $index => $k)
                 <tr>
+                    <td class="text-center">{{ $index + 1 }}</td>
                     <td>{{ $k->nama_lengkap }}</td>
                     <td>{{ $k->email }}</td>
-                    <td>{{ $k->posisi }}</td>
-                    <td>{{ $k->departemen }}</td>
-                    <td>{{ $k->status }}</td>
+                    <td class="text-center">{{ $k->posisi }}</td>
+                    <td class="text-center">{{ $k->departemen }}</td>
+                    <td class="text-center">{{ $k->status }}</td>
                     <td>
                         <a href="{{ route('karyawan.edit', $k) }}" class="btn btn-warning">Edit</a>
                     </td>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -71,15 +71,27 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden text-black">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
+        <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden text-black">
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('karyawan.index')" :active="request()->routeIs('karyawan.index')">
+                {{ __('Karyawan Baru') }}
+            </x-responsive-nav-link>
+        </div>
+        <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden text-black">
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('karyawan.index')" :active="request()->routeIs('karyawan.index')">
+                {{ __('Karyawan') }}
+            </x-responsive-nav-link>
+        </div>
 
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-100 dark:border-gray-600">
+        <div class="pt-4 pb-1 border-t border-gray-100 dark:border-gray-600 text-black">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
@@ -91,7 +103,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
-                            class="text-black"
+                            class="text-white"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
