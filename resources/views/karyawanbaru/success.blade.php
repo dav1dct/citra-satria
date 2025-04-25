1dcb35119d76489c9c439dbb1d16b0754b78bae4
dav1dct <lindahwatih215@gmail.com>
2025-04-25 13:44:05 +0700

    Update success.blade.php

--- resources/views/karyawanbaru/success.blade.php
+++ resources/views/karyawanbaru/success.blade.php
@@ -25,7 +25,7 @@
             <!-- Gambar -->
             <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-[#161615] dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
                 <!-- Tulisan -->
-                <h1 class="font-bold mb-6 text-gray-900 dark:text-white" style="sans-serif; font-weight: bold; font-size: 100px; text-align: center; line-height: 1.0;">
+                <h1 class="font-bold mb-6 text-gray-900 dark:text-white" style="sans-serif; font-weight: bold; font-size: 80px; text-align: center; line-height: 1.0;">
                     Daftar <br>
                     Berhasil<br>
                 </h1>
