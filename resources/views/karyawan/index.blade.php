029f2f58f3f948420faa6b74cb26436cb0b54932
dav1dct <121503738+dav1dct@users.noreply.github.com>
2026-03-05 09:29:00 +0700

    Update index.blade.php

--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -9,19 +9,12 @@
         </div>
     @endif
 <<<<<<< HEAD:resources/views/karyawan/index.blade.php
-<<<<<<< HEAD:resources/views/karyawan/index.blade.php
-<<<<<<< HEAD:resources/views/karyawan/index.blade.php
     @if(auth()->user()->role === 'hsd')
     <a href="{{ route('karyawan.create') }}" class="btn btn-primary mb-3">Tambah Karyawan</a>
     @endif
-    @if(in_array(auth()->user()->role, ['hsd']))
-=======
-=======
->>>>>>> parent of b634018 (Fixed missing tambah karyawan button on karyawan.index):resources/views/admin/karyawan/index.blade.php
-=======
 >>>>>>> parent of b634018 (Fixed missing tambah karyawan button on karyawan.index):resources/views/admin/karyawan/index.blade.php
     @if(in_array(auth()->user()->role, ['admin', 'hsd']))
->>>>>>> parent of b634018 (Fixed missing tambah karyawan button on karyawan.index):resources/views/admin/karyawan/index.blade.php
+
     <div class="mb-3">
         <a href="{{ route('karyawan.export') }}" class="btn btn-success">
             <i class="fas fa-file-excel"></i> Export ke Excel
